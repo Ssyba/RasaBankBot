dbe350da8b51f902e6fcef8828f4e7f2a417b32c
SantaM <tmnifex@gmail.com>
2023-02-16 23:04:47 +0200

    Added submit_pineapple_example_form to domain. Added submit_pineapple_example_form to pineapple_example_form.py Fixed and added submit_pineapple_example_form to the Submit pineapple form.

--- actions/pineapple_example_form.py
+++ actions/pineapple_example_form.py
@@ -23,3 +23,31 @@ class ValidatePineappleExampleForm(FormValidationAction):
             return {"pineapple_slot": user_input}
         else:
             return {"pineapple_slot": None}
+
+
+class SubmitPineappleExampleForm(Action):
+    def name(self) -> Text:
+        return "submit_pineapple_example_form"
+
+    def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        return self.submit(dispatcher, tracker, domain)
+
+    def submit(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        # Perform any custom actions here after form submission
+        # For example, you can retrieve the values of the slots and save them to a database or file
+        pineapple_slot_value = tracker.get_slot("pineapple_slot")
+        if pineapple_slot_value == STOP:
+            dispatcher.utter_message("Form has been stopped, no slots filled.")
+            return [SlotSet("pineapple_slot", None)]
+        dispatcher.utter_message("Submitted pineapple example form.")
+        return [SlotSet("pineapple_slot", pineapple_slot_value)]
